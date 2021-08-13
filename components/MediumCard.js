7706befdde21111aa570fa664aebc55bf8a10e8e
sanketshelar <80213674+sanketshelar@users.noreply.github.com>
2021-08-13 22:25:55 +0530

    airbnb homepage

--- components/MediumCard.js
+++ components/MediumCard.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const MediumCard = ({ img, title }) => {
   return (
     <div className='cursor-pointer hover:scale-105 transition transform duration-200 ease-out'>
-      <div className='relative h-96 w-96'>
+      <div className='relative h-[22rem] w-[22rem]'>
         <Image src={img} layout='fill' className='rounded-lg' />
       </div>
       <h3 className='text-xl font-semibold mt-3'>{title}</h3>
